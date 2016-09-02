ddcdb337b4ea9facf756e00ad3b51b5f82dd32f9
zhangweican <515632400@qq.com>
2016-09-02 16:51:59 +0800

    缓存的生成机制，暂时不适用md5加密

--- trunk/src/main/java/com/leweiyou/cache/BaseKeyGenerator.java
+++ trunk/src/main/java/com/leweiyou/cache/BaseKeyGenerator.java
@@ -9,7 +9,9 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.log4j.Logger;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 
 import com.leweiyou.tools.utils.MD5;
 
@@ -19,30 +21,32 @@ import com.leweiyou.tools.utils.MD5;
  *
  */
 public class BaseKeyGenerator implements KeyGenerator {  
-  
+	private static Logger logger = Logger.getLogger(BaseKeyGenerator.class);
+	
     @Override  
     public Object generate(Object target, Method method, Object... params) {  
   
         StringBuffer buffer = new StringBuffer();  
         Class entityClass = target.getClass();  
-        buffer.append(entityClass.getName());  
+        buffer.append(entityClass.getName() + ".");  
         buffer.append(method.getName());
-        if (params != null && params.length > 1) {  
+        if (params != null && params.length > 0) {  
             for (Object obj : params) {  
                 if (obj != null) {  
                     if (obj instanceof AtomicInteger || obj instanceof AtomicLong || obj instanceof BigDecimal  
                             || obj instanceof BigInteger || obj instanceof Byte || obj instanceof Double  
                             || obj instanceof Float || obj instanceof Integer || obj instanceof Long  
                             || obj instanceof Short) {  
-                        buffer.append(obj);  
+                        buffer.append("." + obj.getClass().getSimpleName() + "." + obj);  
                     } else if (obj instanceof List || obj instanceof Set || obj instanceof Map) {  
-                        buffer.append(obj);  
+                        buffer.append("." + obj.getClass().getSimpleName() + "." + obj);  
                     } else {  
-                        buffer.append(obj.hashCode());  
+                        buffer.append("." + obj.getClass().getSimpleName() + "." + obj.hashCode());  
                     }  
                 }  
             }  
         }  
+        logger.debug("KeyGenerator:" + buffer.toString() + " - size:" + (params == null ? 0 : params.length));
         return buffer.toString();  
     }  
   
